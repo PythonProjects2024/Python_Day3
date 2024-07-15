75a189ee62c34512736f2c8e8232ce4639013e52
Dawood Rizwan <dawood.rizwan@outlook.com>
2024-07-15 14:56:12 +0200

    Add project files.

---
+++ Python_Day3/Python_Day3.py
@@ -0,0 +1,104 @@
+﻿
+# Conditional Statements:
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# # Comparison Operators
+# # > Greater than
+# # < Less than
+# # >= Greater than or equal to
+# # <= Less than or equal to
+# # == Equal to
+# # != Not equal to
+    
+# print()
+
+
+# # Which number do you want to check?
+# number = int(input())
+# # 🚨 Don't change the code above 👆
+
+# # Write your code below this line 👇
+# if number % 2 == 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+  
+# from telnetlib import EL
+
+
+# print()
+
+
+
+
+# print("Welcome to the Pytho Theme Park")
+# age = int(input("Please enter you age. "))
+# height = int(input("Please enter you height. "))
+
+# if age < 3 and age <60:
+#    print("You are free to park.")
+
+# elif height < 120:
+#    print("you cannot ride this rollercoaster.")
+# elif height > 120:
+#      print("you can ride this rollercoaster.")
+
+   
+# else:
+#    print("You have to pay $20")
+
+# print()
+
+
+# BMI Calculator 
+
+# Enter your height in meters e.g., 1.55
+# height = float(input())
+# # Enter your weight in kilograms e.g., 72
+# weight = int(input())
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# square_meter = float(height) ** 2
+# bmi = int(weight) / float(square_meter )
+# bmi_as_int = float(bmi)
+
+# if bmi_as_int  < 18.5:
+#   print(f"Your BMI is {bmi_as_int}, you are underweight. ")
+# elif bmi_as_int >= 18.5 and  bmi_as_int < 25:
+#     print(f"Your BMI is {bmi_as_int}, you have a normal weight. ")
+# elif bmi_as_int >= 25 and bmi_as_int < 30:
+#     print(f"Your BMI is {bmi_as_int}, you are slightly overweight. ")
+# elif bmi_as_int >= 30 and bmi_as_int < 35:
+#     print(f"Your BMI is {bmi_as_int}, you are obese. ")
+# elif bmi_as_int == 35 and bmi_as_int > 35:
+#     print(f"Your BMI is {bmi_as_int}, you are a clinically obese. ")
+
+# print()
+
+
+# Leap Year
+
+# Which year do you want to check?
+year = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 ==0:
+  print("Leap year")
+else:
+  print("Not leap year")
+  
+
+
+
+
+
